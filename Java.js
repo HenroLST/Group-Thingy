9d56bdeb5fd9c5c80983356d02d090b8aa239dc0
unknown <epicpizza312@gmail.com>
2021-08-03 11:27:48 -0500

    fixed breaks and delay

--- Java.js
+++ Java.js
@@ -4,29 +4,46 @@ var pos=0;
 const sleep = (time) => {
     return new Promise(resolve => setTimeout(resolve, time))
   }
+  var shoot = async function(){
+    bull.style.visibility="visible"
+    bull.style.left=el.offsetLeft+"px"
+    bull.style.top=el.offsetTop+"px"
+    
+    for(var i=el.offsetTop;i>-100;i-=10){
 
-document.addEventListener("keydown",async function(event){
+        bull.style.top=i+"px"
 
-    if(event.keyCode==65){
+        await sleep()
+    
+    }
+}
+  var tickRate = 30,
+  keyDown = {},
+  keyMap = {
+      65: 'left',
+      68: 'right',
+      32: 'space',
+
+  };
+document.addEventListener("keydown",function(e){keyDown[keyMap[e.which]]=true;})
+document.addEventListener("keyup",function(e){keyDown[keyMap[e.which]]=false;})
+var move =  async function(){
+
+    if(keyDown['left']){
         el.style.left=el.offsetLeft-10+'px'
         
         
     }
-    if(event.keyCode==68){
+    if(keyDown['right']){
         el.style.left=el.offsetLeft+10+'px'
     }
-    if(event.keyCode==32){
-        bull.style.visibility="visible"
-        bull.style.left=el.offsetLeft+"px"
-        bull.style.top=el.offsetTop+"px"
-        
-        for(var i=el.offsetTop;i>-100;i-=10){
+    if(keyDown['space']){
 
-            bull.style.top=i+"px"
-
-            await sleep()
+        shoot()
         
-        }
 
     }
-});
+    setTimeout(move, tickRate);
+
+};
+move();
