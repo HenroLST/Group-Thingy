f589bb14692e8f58573162f79d504333ad24b7ac
unknown <epicpizza312@gmail.com>
2021-08-03 10:47:00 -0500

    bruh

---
+++ Java.js
@@ -0,0 +1,32 @@
+var el=document.getElementById("shooter");
+var bull=document.getElementById("bullet");
+var pos=0;
+const sleep = (time) => {
+    return new Promise(resolve => setTimeout(resolve, time))
+  }
+
+document.addEventListener("keydown",async function(event){
+
+    if(event.keyCode==65){
+        el.style.left=el.offsetLeft-10+'px'
+        
+        
+    }
+    if(event.keyCode==68){
+        el.style.left=el.offsetLeft+10+'px'
+    }
+    if(event.keyCode==32){
+        bull.style.visibility="visible"
+        bull.style.left=el.offsetLeft+"px"
+        bull.style.top=el.offsetTop+"px"
+        
+        for(var i=el.offsetTop;i>-100;i-=10){
+
+            bull.style.top=i+"px"
+
+            await sleep()
+        
+        }
+
+    }
+});
