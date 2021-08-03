1f0cf13e717ab855c3c63f37c45da5c50ce5658e
HenroLST <87709518+HenroLST@users.noreply.github.com>
2021-08-03 12:00:21 -0500

    Update Java.js

--- Lvl1/Java.js
+++ Lvl1/Java.js
@@ -4,39 +4,37 @@ var pos=0;
 const sleep = (time) => {
     return new Promise(resolve => setTimeout(resolve, time))
   }
-
-document.addEventListener("keydown",async function(event){
-
-    if(event.keyCode==65){
-        el.style.left=el.offsetLeft-10+'px'
-
-
+  var shoot =  function(){
+   document.addEventListener("keyup",async function(event){
+        if(event.keyCode==32){
+            console.log("bruh")
+            bull.style.visibility="visible"
+            bull.style.left=el.offsetLeft+"px"
+            bull.style.top=el.offsetTop+"px"
+            for(var i=el.offsetTop;i>-100;i-=10){
+                bull.style.top=i+"px"
+                await sleep()
+        }
     }
-    if(event.keyCode==68){
+} );
+  }
+  var tickRate = 30,
+  keyDown = {},
+  keyMap = {
+      65: 'left',
+      68: 'right',
+      32: 'space',
+  };
+document.addEventListener("keydown",function(e){keyDown[keyMap[e.which]]=true;})
+document.addEventListener("keyup",function(e){keyDown[keyMap[e.which]]=false;})
+var move =  async function(){
+    if(keyDown['left']){
+        el.style.left=el.offsetLeft-10+'px'
+    }
+    if(keyDown['right']){
         el.style.left=el.offsetLeft+10+'px'
     }
-    if(event.keyCode==32){
-        bull.style.visibility="visible"
-        bull.style.left=el.offsetLeft+"px"
-        bull.style.top=el.offsetTop+"px"
-
-        for(var i=el.offsetTop;i>-100;i-=10){
-
-            bull.style.top=i+"px"
-
-            await sleep()
-
-        }
-
-    }
-    var topenemynumb = document.getElementById("topenemies");
-    var middleenemynumb = document.getElementById("middleenemies");
-    var bottomnemynumb = document.getElementById("bottomenemies");
-
-if (topenemynumb == 2){
-    topenemynumb++;
-}
-
-
-
-}); 
+    shoot()
+    setTimeout(move, tickRate);
+};
+move();
