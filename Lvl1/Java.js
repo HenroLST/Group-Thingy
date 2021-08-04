5d3c145077ab52f22cf36e7f0f89defa78044565
Henry Zhao <zhao.henro14@gmail.com>
2021-08-04 10:46:41 -0500

    GroupProject

--- Lvl1/Java.js
+++ Lvl1/Java.js
@@ -4,20 +4,32 @@ var pos=0;
 const sleep = (time) => {
     return new Promise(resolve => setTimeout(resolve, time))
   }
-  var shoot =  function(){
-   document.addEventListener("keyup",async function(event){
+ 
+  document.addEventListener("keyup",async function(event){
+      if(bull.offsetTop==el.offsetTop){
+          console.log(bull.offsetTop)
+
         if(event.keyCode==32){
-            console.log("bruh")
+            
+            
             bull.style.visibility="visible"
             bull.style.left=el.offsetLeft+"px"
-            bull.style.top=el.offsetTop+"px"
+ 
             for(var i=el.offsetTop;i>-100;i-=10){
+
                 bull.style.top=i+"px"
                 await sleep()
+            
+            }
+                                
+            
+            bull.style.visibility="hidden"
+            bull.style.top=el.offsetTop+"px"  
+            
         }
-    }
-} );
-  }
+      }
+});
+
   var tickRate = 30,
   keyDown = {},
   keyMap = {
@@ -28,13 +40,32 @@ const sleep = (time) => {
 document.addEventListener("keydown",function(e){keyDown[keyMap[e.which]]=true;})
 document.addEventListener("keyup",function(e){keyDown[keyMap[e.which]]=false;})
 var move =  async function(){
-    if(keyDown['left']){
-        el.style.left=el.offsetLeft-10+'px'
+   
+    if((el.offsetLeft>4&&el.offsetLeft<1720)){
+        
+        if(keyDown['left']){
+            el.style.left=el.offsetLeft-10+'px'
+        }
+
+        if(keyDown['right']){
+            el.style.left=el.offsetLeft+10+'px'
+        }
+        
     }
-    if(keyDown['right']){
-        el.style.left=el.offsetLeft+10+'px'
+    else if(el.offsetLeft<=4){
+        if(keyDown['right']){
+            el.style.left=el.offsetLeft+10+'px'
+        }
+    
     }
-    shoot()
+    else if(el.offsetLeft>=1720){
+        if(keyDown['left']){
+            el.style.left=el.offsetLeft-10+'px'
+        }
+    
+    }
+
+
     setTimeout(move, tickRate);
 };
 move();
